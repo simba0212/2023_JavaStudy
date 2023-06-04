18624bea4de03a1e58e62d60e8f5b499afb72eed
Simba Kim <ksimba0212@naver.com>
2023-06-04 23:15:33 +0900

    0604집

--- src/hw09/Hw_JTable.java
+++ src/hw09/Hw_JTable.java
@@ -3,24 +3,61 @@ package hw09;
 import javax.swing.table.AbstractTableModel;
 
 public class Hw_JTable extends AbstractTableModel{
-
-	@Override
-	public int getRowCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int getColumnCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public Object getValueAt(int rowIndex, int columnIndex) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 	
+	
+
+	// 1. 컬럼명(열의 대표이름)을 배열로 만든다.
+		String[] name = { "ID", "Name", "Phone", "Address", "Company" };
+
+		String[][] data = { 
+				{ "100", "dada", "010-4554-9851", "seoul", "cj" },
+				{ "101", "jenho", "010-4554-9851", "seoul", "asdf" },
+				{ "102", "cccc", "010-4554-9851", "seoul", "ict" },
+				{ "103", "dddd", "010-4554-9851", "seoul", "iaefs" },
+				
+		
+		};
+
+		// 행의 수 = rows
+		@Override
+		public int getRowCount() {
+			return data.length;
+		}
+
+		// 열의 수 = Column
+		@Override
+		public int getColumnCount() {
+			return name.length;
+		}
+
+		// 내용 삽입
+		@Override
+		public Object getValueAt(int rowIndex, int columnIndex) {
+			return data[rowIndex][columnIndex];
+		}
+
+		// 열의 이름설정
+		// ** 스크롤패인에 추가하지 않으면, 이믈삽입이 안된다 **
+		@Override
+		public String getColumnName(int column) {
+			return name[column];
+		}
+
+		// 내용 수정
+		@Override
+
+		public boolean isCellEditable(int rowIndex, int columnIndex) {
+			if (columnIndex != 0) {
+				return true;
+			}else {
+				return false;
+			}
+		}
+
+		// 고친내용 적용하기
+		@Override
+		public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+			data[rowIndex][columnIndex] = (String) aValue;
+		}
 
 }
--- src/hw09/Hw_main.java
+++ src/hw09/Hw_main.java
@@ -1,53 +1,111 @@
 package hw09;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
 
-public class Hw_main extends JFrame{
-	JPanel jp1;
-	JTextField id,name,phone,addr,comp;
-	JButton add,del,edit,search,clean,seeA;
-	
-	
+import day17.com.ict.edu3.Ex01_JTable;
+
+public class Hw_main extends JFrame {
+	JPanel jp1, jp2, jp3, jp4, jp01;
+	JPanel t1,t2,t3,t4,t5;
+	JTextField id, name, phone, addr, comp, searchF;
+	JButton add, del, edit, searchB, clean, seeA;
+	JTextArea jta;
+	JComboBox<String> box;
+	JTable table;
+
 	public Hw_main() {
-		super("");
-		add(new JLabel("Address Book",JLabel.CENTER),BorderLayout.NORTH);
+		super("DB 연동정보");
+		jp01 = new JPanel(new FlowLayout(FlowLayout.CENTER));
+		jp1 = new JPanel(new GridLayout(5,0));
+
+		t1 = new JPanel();
+		t2 = new JPanel();
+		t3 = new JPanel();
+		t4 = new JPanel();
+		t5 = new JPanel();
 		
-		jp1 = new JPanel(new GridLayout(5,2));
-		jp1.add(new JLabel("I D :",JLabel.CENTER));
 		id = new JTextField(20);
-		jp1.add(id);
-		jp1.add(new JLabel("이 름 :",JLabel.CENTER));
+		t1.add(new JLabel("ID     : "));
+		t1.add(id);
+		jp1.add(t1);
+		
+
 		name = new JTextField(20);
-		jp1.add(name);
-		jp1.add(new JLabel("전 화 :",JLabel.CENTER));
+		t2.add(new JLabel("이름 : "));
+		t2.add(name);
+		jp1.add(t2);
+
 		phone = new JTextField(20);
-		jp1.add(phone);
-		jp1.add(new JLabel("주 소 :",JLabel.CENTER));
+		t3.add(new JLabel("전화 : "));
+		t3.add(phone);
+		jp1.add(t3);
+
 		addr = new JTextField(20);
-		jp1.add(addr);
-		jp1.add(new JLabel("회 사 :",JLabel.CENTER));
+		t4.add(new JLabel("주소 : "));
+		t4.add(addr);
+		jp1.add(t4);
+		
 		comp = new JTextField(20);
-		jp1.add(comp);
+		t5.add(new JLabel("회사 : "));
+		t5.add(comp);
+		jp1.add(t5);
 		
-		
-		
-		
-		add(jp1);
-		
-		setSize(600, 450);
-		setVisible(true);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		jp01.add(jp1);
+
+		jp2 = new JPanel();
+
+		add = new JButton("추가");
+		del = new JButton("삭제");
+		edit = new JButton("수정");
+
+		String[] items = { "ID", "이름", "전화", "주소", "회사" };
+		box = new JComboBox<>(items);
+		searchF = new JTextField(10);
+
+		searchB = new JButton("검색");
+		clean = new JButton("지우기");
+		seeA = new JButton("전체보기");
+
+		jp2.add(add);
+		jp2.add(del);
+		jp2.add(edit);
+		jp2.add(box);
+		jp2.add(searchF);
+		jp2.add(searchB);
+		jp2.add(clean);
+		jp2.add(seeA);
+
+		jp3 = new JPanel();
+		jp3.add(new JLabel("Address Book"));
+
+		Hw_JTable table = new Hw_JTable();
+		JTable jtable = new JTable(table);
+		JScrollPane jsp = new JScrollPane(jtable);
+
+		add(jp01, BorderLayout.WEST);
+		add(jp2, BorderLayout.SOUTH);
+		add(jp3, BorderLayout.NORTH);
+		add(jsp, BorderLayout.EAST);
+
+		pack();
 		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
 	}
-	
-	
+
 	public static void main(String[] args) {
 		new Hw_main();
 	}
